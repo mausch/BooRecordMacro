86bf9a056de05c6db476c890af4ec62c3a7b1b85
mausch <mauricioscheffer@gmail.com>
2013-02-18 19:31:24 -0300

    Boo - Visual Studio integration is broken. Using batch script to compile

---
+++ build.bat
@@ -0,0 +1,6 @@
+@echo off
+lib\booc.exe RecordMacros\RecordMacro.boo -r:lib/fsharpx.core.dll  -o:RecordMacros\bin\Debug\RecordMacros.dll
+lib\booc.exe Example\LensExtensions.boo Example\Program.boo -r:lib/fsharpx.core.dll -r:RecordMacros\bin\Debug\RecordMacros.dll -o:Example\bin\debug\Example.exe
+copy lib\Boo.lang.dll Example\bin\debug\
+copy lib\FSharpx.Core.dll Example\bin\debug\
+copy RecordMacros\bin\Debug\RecordMacros.dll Example\bin\debug\
